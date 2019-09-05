fff8ba337fd59685d422a9fd942ee337741c03d8
Иван Хорошевский <butoxors@gmail.com>
2019-09-05 20:44:41 +0300

    +prettry pagination

--- Store/HtmlHelpers/PagingHelper.cs
+++ Store/HtmlHelpers/PagingHelper.cs
@@ -12,20 +12,63 @@ namespace Store.HtmlHelpers
                                               Func<int, string> pageUrl)
         {
             StringBuilder result = new StringBuilder();
+
+            TagBuilder pagination = new TagBuilder("ul");
+
+            pagination.AddCssClass("pagination");
+
+            string link = "";
+
+            if (pagingInfo.CurrentPage != 1)
+                link = CreateLink("Previous", pageUrl, pagingInfo.CurrentPage - 1);
+
+            pagination.InnerHtml += link;
+
             for (int i = 1; i <= pagingInfo.TotalPages; i++)
             {
-                TagBuilder tag = new TagBuilder("a");
-                tag.MergeAttribute("href", pageUrl(i));
-                tag.InnerHtml = i.ToString();
+                link = "";
                 if (i == pagingInfo.CurrentPage)
-                {
-                    tag.AddCssClass("selected");
-                    tag.AddCssClass("btn-primary");
-                }
-                tag.AddCssClass("btn btn-default");
-                result.Append(tag.ToString());
+                    link = CreateLink(i.ToString(), pageUrl, i, true);
+                else
+                    link = CreateLink(i.ToString(), pageUrl, i);
+
+                pagination.InnerHtml += link;
             }
+
+            link = "";
+
+            if (pagingInfo.CurrentPage != pagingInfo.TotalPages)
+                link = CreateLink("Next", pageUrl, pagingInfo.CurrentPage + 1);
+
+            pagination.InnerHtml += link;
+
+            result.Append(pagination.ToString());
+
             return MvcHtmlString.Create(result.ToString());
         }
+
+        private static string CreateLink(string text, Func<int, string> pageUrl, int number, bool active = false)
+        {
+            TagBuilder li = new TagBuilder("li");
+            TagBuilder a = new TagBuilder("a");
+
+            li.AddCssClass("page-item");
+
+            a.MergeAttribute("href", pageUrl(number));
+
+            a.InnerHtml = text;
+            a.AddCssClass("page-link");
+
+            if (active)
+            {
+                a.AddCssClass("selected");
+                a.AddCssClass("btn-primary");
+            }
+
+            li.InnerHtml += a.ToString();
+
+            return li.ToString();
+        }
     }
+    
 }
